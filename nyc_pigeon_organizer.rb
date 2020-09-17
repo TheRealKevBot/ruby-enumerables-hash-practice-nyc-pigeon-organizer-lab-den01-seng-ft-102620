8f7ef11c95e8daa7b4c0a939be4bcce91cba4bf0
Kevin Glasgow <kglas001@gmail.com>
2020-09-17 22:02:23 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,14 @@
+require "pry"
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {} 
+  data.each_with_object({}) do |(keys, values), pigeon_list|
+    values.each do |key_name, names|
+      names.each do |name|
+        pigeon_list << name
+        pigeon_list.uniq 
+        binding.pry
+      end
+    end
+  end
+  binding.pry
 end
