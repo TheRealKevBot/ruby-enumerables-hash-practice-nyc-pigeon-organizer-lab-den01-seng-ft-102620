d2d89c55a275f044baee5a326c47356a9422ec0d
Kevin Glasgow <kglas001@gmail.com>
2020-09-17 22:35:20 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,6 +4,8 @@ def nyc_pigeon_organizer(data)
   data.each_with_object({}) do |(keys, values), pigeon_list|
     values.each do |key_name, names|
       names.each do |name|
+        pigeon_list << name
+        pigeon_list.uniq
         binding.pry
       end
     end
